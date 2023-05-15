b857a5410c172a0a271e51ad7260a321b93efdb9
JEONGMUN LEE <74344761+leejeongmunn@users.noreply.github.com>
2023-05-15 23:30:41 +0900

    <Add> get pages num

---
+++ main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/PuerkitoBio/goquery"
+)
+
+
+var baseURL string = "https://www.saramin.co.kr/zf_user/search/recruit?&searchword=python"
+
+func main() {
+	totalPages := getPages()
+	fmt.Println(totalPages)
+
+
+
+}
+
+
+func getPages() int {
+	pages := 0
+	res, err := http.Get(baseURL)
+	checkErr(err)
+	checkCode(res)
+
+	defer res.Body.Close()
+
+
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	doc.Find(".pagination").Each(func(i int, s*goquery.Selection) {
+		pages = s.Find("a").Length()
+	})
+	return pages
+}
+
+func checkErr(err error) {
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
+
+func checkCode(res *http.Response) {
+	if res.StatusCode != 200 {
+		log.Fatalln("Request failed with Status Code :", res.StatusCode)
+
+	}
+}
